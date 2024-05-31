5f338fd0b676206b62fa5eb385f168a2f31761e4
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2024-05-31 15:44:31 +0700

    add product model

---
+++ api/models/productModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const productSchma = new mongoose.Schema({
+    productName : {
+        type : String,
+        required : true,
+    },
+    productType : {
+        type : String,
+        required : true,
+    },
+    productQty : {
+        type : Number,
+        required : true,
+    },
+    price : {
+        type : Number,
+        required : true,
+    },
+    imagePath : {
+        type : String,
+        required : true,
+    },
+    description :{
+        type : String,
+        required : true
+    }
+}, {timestamps : true});
+
+const Product = mongoose.model('Product' , productSchma);
+module.exports = Product;
